033c09a62323610d3f6e07750e838a01a3fccad0
JHaarla <haarladesign@gmail.com>
2019-10-29 16:17:29 -0400

    added validations to the User model

--- models/example.js
+++ models/example.js
@@ -1,8 +1,26 @@
 module.exports = function(sequelize, DataTypes) {
   var User = sequelize.define("User", {
-    userName: DataTypes.STRING,
-    password: DataTypes.STRING,
-    email: DataTypes.STRING
+    userName: {
+      type: DataTypes.STRING,
+      allowNull: false, // do not allow null
+      validate: {
+        len: [1] //minimum 1 character
+      }
+    },
+
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false, // do not allow null/empty
+      validate: {
+        len: [4] // minimum 4 characters
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true //checks for proper email format
+      }
+    }
   });
   return User;
 };
